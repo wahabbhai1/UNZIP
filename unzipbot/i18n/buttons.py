cc17d344d77709da0bffbf2a212dd81e6b8d7708
wahabbhai1 <wb7257490@gmail.com>
2026-02-06 08:06:01 +0530

    Add files via upload

---
+++ unzipbot/i18n/buttons.py
@@ -0,0 +1,280 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from unzipbot.helpers.database import get_lang
+
+from .messages import Messages
+
+messages = Messages(lang_fetcher=get_lang)
+
+
+# Inline buttons
+class Buttons:
+    START_BUTTON = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="HELP"),
+                    callback_data="helpcallback",
+                ),
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="ABOUT"),
+                    callback_data="aboutcallback",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="STATS_BTN"),
+                    callback_data="statscallback",
+                ),
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="DONATE"),
+                    callback_data="donatecallback",
+                ),
+            ],
+        ]
+    )
+
+    REFRESH_BUTTON = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="REFRESH"),
+                    callback_data="statscallback|refresh",
+                ),
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="BACK"),
+                    callback_data="megoinhome",
+                ),
+            ]
+        ]
+    )
+
+    CHOOSE_E_F__BTNS = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="🗂️", callback_data="extract_file|tg_file|no_pass"
+                ),
+                InlineKeyboardButton(
+                    text="🔐", callback_data="extract_file|tg_file|with_pass"
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="🖼️", callback_data="extract_file|tg_file|thumb"
+                ),
+                InlineKeyboardButton(
+                    text="✏", callback_data="extract_file|tg_file|thumbrename"
+                ),
+            ],
+            [InlineKeyboardButton(text="❌", callback_data="cancel_dis")],
+        ]
+    )
+
+    CHOOSE_E_F_M__BTNS = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text="🗂️", callback_data="merged|no_pass"),
+                InlineKeyboardButton(text="🔐", callback_data="merged|with_pass"),
+            ],
+            [InlineKeyboardButton(text="❌", callback_data="cancel_dis")],
+        ]
+    )
+
+    CHOOSE_E_U__BTNS = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="🔗", callback_data="extract_file|url|no_pass"
+                ),
+                InlineKeyboardButton(
+                    text="🔐", callback_data="extract_file|url|with_pass"
+                ),
+            ],
+            [
+                InlineKeyboardButton(text="🖼️", callback_data="extract_file|url|thumb"),
+                InlineKeyboardButton(
+                    text="✏", callback_data="extract_file|url|thumbrename"
+                ),
+            ],
+            [InlineKeyboardButton(text="❌", callback_data="cancel_dis")],
+        ]
+    )
+
+    RENAME = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text="✏", callback_data="renameit"),
+                InlineKeyboardButton(text="🙅‍♂️", callback_data="norename"),
+            ]
+        ]
+    )
+
+    CLN_BTNS = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="CLEAN"),
+                    callback_data="cancel_dis",
+                ),
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="CANCEL_IT"),
+                    callback_data="nobully",
+                ),
+            ]
+        ]
+    )
+
+    ME_GOIN_HOME = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="BACK"),
+                    callback_data="megoinhome",
+                )
+            ]
+        ]
+    )
+
+    SET_UPLOAD_MODE_BUTTONS = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="AS_DOC"),
+                    callback_data="set_mode|doc",
+                ),
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="AS_MEDIA"),
+                    callback_data="set_mode|media",
+                ),
+            ]
+        ]
+    )
+
+    I_PREFER_STOP = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="CANCEL_IT"),
+                    callback_data="canceldownload",
+                )
+            ]
+        ]
+    )
+
+    MERGE_THEM_ALL = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="MERGE_BTN"),
+                    callback_data="merge_this",
+                ),
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="CANCEL_IT"),
+                    callback_data="cancel_dis",
+                ),
+            ]
+        ]
+    )
+
+    THUMB_REPLACEMENT = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="CHECK"),
+                    callback_data="check_thumb",
+                ),
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="REPLACE"),
+                    callback_data="save_thumb|replace",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="CANCEL_IT"),
+                    callback_data="nope_thumb",
+                )
+            ],
+        ]
+    )
+
+    THUMB_FINAL = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="REPLACE"),
+                    callback_data="save_thumb|replace",
+                ),
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="CANCEL_IT"),
+                    callback_data="nope_thumb",
+                ),
+            ]
+        ]
+    )
+
+    THUMB_SAVE = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="SAVE"),
+                    callback_data="save_thumb|save",
+                ),
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="CANCEL_IT"),
+                    callback_data="nope_thumb",
+                ),
+            ]
+        ]
+    )
+
+    THUMB_DEL = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="CHECK"),
+                    callback_data="check_before_del",
+                ),
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="DELETE"),
+                    callback_data="del_thumb",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="CANCEL_IT"),
+                    callback_data="nope_thumb",
+                )
+            ],
+        ]
+    )
+
+    THUMB_DEL_2 = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="DELETE"),
+                    callback_data="del_thumb",
+                ),
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="CANCEL_IT"),
+                    callback_data="nope_thumb",
+                ),
+            ]
+        ]
+    )
+
+    RATE_ME = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="RATE"),
+                    url="https://t.me/BotsArchive/2705",
+                ),
+                InlineKeyboardButton(
+                    text=messages.get(file="buttons", key="DONATE"),
+                    callback_data="donatecallback",
+                ),
+            ]
+        ]
+    )
---
+++ unzipbot/i18n/messages.py
@@ -0,0 +1,65 @@
+import json
+
+from config import Config
+
+
+class Messages:
+    def __init__(
+        self,
+        lang_fetcher=None,
+        default_lang=Config.BASE_LANGUAGE,
+        base_path="unzipbot/i18n/lang",
+    ):
+        """
+        Initialize the Messages class
+
+        :param lang_fetcher: A callable to fetch the user's language (takes user_id as argument)
+        :param default_lang: The default language code (ex "en")
+        :param base_path: The base path to the directory containing language files
+        """  # noqa: E501
+        self.lang_fetcher = lang_fetcher or (lambda _: default_lang)
+        self.default_lang = default_lang
+        self.base_path = base_path
+
+    def __load_language_file(self, lang):
+        """
+        Load the JSON file for the given language
+
+        :param lang: Language code (ex "en")
+        :return: Dictionary of messages
+        """
+        file_path = f"{self.base_path}/{lang}.json"
+
+        try:
+            with open(file=file_path, mode="r", encoding="utf-8") as f:
+                return json.load(fp=f)
+        except FileNotFoundError:
+            with open(
+                file=f"{self.base_path}/{self.default_lang}.json",
+                mode="r",
+                encoding="utf-8",
+            ) as f:
+                return json.load(fp=f)
+
+    def get(self, file, key, user_id=None, extra_args=[]):
+        """
+        Retrieve and format a message by its file and key
+
+        :param file: The name of the file in the JSON structure
+        :param key: The key within the file to retrieve
+        :param user_id: The user's ID (used to fetch the preferred language)
+        :param extra_args: Additional arguments for string formatting
+        :return: The formatted message string
+        """
+        lang = self.lang_fetcher(user_id) if user_id else self.default_lang
+        messages = self.__load_language_file(lang)
+
+        try:
+            message = messages[file][key.lower()]
+        except KeyError:
+            message = self.__load_language_file(self.default_lang)[file][key.lower()]
+
+        if not isinstance(extra_args, list):
+            extra_args = [extra_args]
+
+        return message.format(*extra_args)
